a7998aabcf35bbe271f78ab80fd51c59f0ac2714
Cosmin Apreutesei <cosmin.apreutesei@gmail.com>
2015-06-24 16:31:21 +0300

    unimportant

--- heap.lua
+++ heap.lua
@@ -2,116 +2,125 @@
 --Priority queue implemented as a binary heap.
 --Written by Cosmin Apreutesei. Public Domain.
 
-local assert, floor, insert, remove =
-	assert, math.floor, table.insert, table.remove
+--heap algorithm working over abstract API that counts from one.
 
-local function heapfactory(add, remove)
+local assert, floor = assert, math.floor
 
-end
+local function heap(add, rem, get, set, less, length)
 
-local heap = {}
-heap.__index = heap
-
-function heap:push(val)
-	assert(val ~= nil, 'invalid value')
-	local t = self.t
-	self:_add(t, val)
-	local child = self:length()
-	local parent = floor(child / 2)
-	while child > 1 and self.cmp(t[child], t[parent]) do
-		t[child], t[parent] = t[parent], t[child]
-		child = parent
-		parent = floor(child / 2)
+	local function swap(i1, i2)
+		local v1 = get(i1)
+		set(i1, get(i2))
+		set(i2, v1)
 	end
-end
 
-function heap:pop()
-	local t = self.t
-	if self:length() < 2 then
-		return self:_remove(t)
-	end
-	local root = 1
-	local val = t[root]
-	t[root] = self:_remove(t) --replace root with last value
-	local size = self:length()
-	if size > 1 then --move root back to its place
-		local child = 2 * root
-		while child <= size do
-			if child + 1 <= size and self.cmp(t[child+1], t[child]) then
-				child = child + 1
-			end
-			if self.cmp(t[child], t[root]) then
-				t[root], t[child] = t[child], t[root]
-				root = child
-			else
-				break
-			end
-			child = 2 * root
+	local function push(val)
+		add(val)
+		local child = length()
+		local parent = floor(child / 2)
+		while child > 1 and less(child, parent) do
+			swap(child, parent)
+			child = parent
+			parent = floor(child / 2)
 		end
 	end
-	return val
+
+	local function pop()
+		if length() < 2 then
+			return rem()
+		end
+		local val = get(1)
+		set(1, rem()) --replace root with last value
+		local root = 1
+		local last = length()
+		if last > 1 then --move root back to its place
+			local child = root * 2
+			while child <= last do
+				if child + 1 <= last and less(child + 1, child) then
+					child = child + 1
+				end
+				if less(child, root) then
+					swap(root, child)
+					root = child
+				else
+					break
+				end
+				child = root * 2
+			end
+		end
+		return val
+	end
+
+	return push, pop
 end
 
-function heap:peek()
-	return self.t[1]
+--cdata heap working over a cdata array
+
+local ffi
+
+local function cdataheap(h)
+	ffi = ffi or require'ffi'
+	assert(h and h.size, 'size expected')
+	assert(h.size > 1, 'invalid size')
+	assert(h.data or h.ctype, 'data or ctype expected')
+	h.data = h.data or ffi.new(ffi.typeof('$[?]', ffi.typeof(h.ctype)), h.size)
+	local cmp = h.cmp or function(a, b) return a < b end
+	local t, n, maxn = h.data, h.length or 0, h.size-1
+	local function add(v) assert(n < maxn, 'buffer overflow'); n=n+1; t[n]=v end
+	local function rem() assert(n >= 1, 'buffer underflow'); local v=t[n]; n=n-1; return v end
+	local function get(i) return t[i] end
+	local function set(i, v) t[i]=v end
+	local function less(i1, i2) return cmp(t[i1], t[i2]) end
+	local function length() return n end
+	local push, pop = heap(add, rem, get, set, less, length)
+	function h:push(val) push(val) end
+	function h:pop() return pop(pop) end
+	function h:peek() return get(1) end
+	h.length = length
+	return h
 end
 
---Lua value heap
+--value heap working over a Lua table
 
-local vheap = {push = heap.push, pop = heap.pop, peek = heap.peek}
-
-function vheap:length() return #self.t end
-function vheap:_add(t, val) insert(t, val) end
-function vheap:_remove(t) remove(t) end
-
-local function new_vheap(t, cmp)
-	t = t or {}
-	cmp = cmp or function(a, b) return a < b end
-	local self = {t = t, cmp = cmp}
-	return setmetatable(self, {__index = vheap})
+local function valueheap(h)
+	h = h or {}
+	h.cmp = h.cmp or function(a, b) return a < b end
+	local t, cmp, tins, trem = h, h.cmp, table.insert, table.remove
+	local function add(v) assert(v ~= nil, 'invalid value'); tins(t, v) end
+	local function rem() assert(#t > 0, 'buffer underflow'); return trem(t) end
+	local function get(i) return t[i] end
+	local function set(i, v) t[i]=v end
+	local function less(i1, i2) return cmp(t[i1], t[i2]) end
+	local function length() return #t end
+	local push, pop = heap(add, rem, get, set, less, length)
+	function h:push(val) push(val) end
+	function h:pop() return pop(pop) end
+	function h:peek() return get(1) end
+	h.length = length
+	return h
 end
 
---cdata heap
-
-local dheap = {push = heap.push, pop = heap.pop, peek = heap.peek}
-
-function dheap:_init(ctype, size)
-	self._len = size
-	self.t = ffi.new(ffi.typeof('$[?]', ffi.typeof(ctype)), size + 1)
-end
-
-function dheap:length()
-	return self._len
-end
-
-function dheap:_add(t, val)
-	self._len = self._len + 1
-	t[self.len] = val
-end
-
-function dheap:_remove(t)
-	self._len = self._len - 1
-end
-
-
-
---unit test
+--test
 
 if not ... then
-	local h = new_vheap()
-	for i = 1, 100000 do
-	    h:push(math.random(1, 100000))
+	local size = 100000
+	local function test(h)
+		for i = 1, size do
+		    h:push(math.random(1, size))
+		end
+		local v0 = h:pop()
+		for i=2,size do
+			local v = h:pop()
+			assert(v >= v0)
+			v0 = v
+		end
 	end
-	local t = {}
-	repeat
-		local n = h:pop()
-		table.insert(t, n)
-	until not n
-	for i = 1, #t-1 do
-		assert(t[i] <= t[i+1])
-	end
+	test(valueheap())
+	test(cdataheap{ctype = 'int', size = size + 1})
 end
 
 return {
-	valueheap = new_vheap,
+	heap = heap,
+	valueheap = valueheap,
+	cdataheap = cdataheap,
 }
--- heap.md
+++ heap.md
@@ -4,33 +4,36 @@ tagline: priority queues
 
 ## `local heap = require'heap'`
 
-Priority queue implemented as a binary heap. Binary heaps are arrays
-for which t[i] <= t[2*i+1] and t[i] <= t[2*i+2] for all indices.
+Priority queue implemented as a binary heap.
 
 ## API
 
-------------------------- ----------------------------------------------------
-heap([t]) -> h            create a heap
-h:push(val)               push a value
-h:pop() -> val            pop highest value
-h:peek() -> val           get highest value without popping
-h:length()                number of elements
-------------------------- ----------------------------------------------------
+--------------------------------------------------------- ----------------------------------------------------
+__algorithm__
+heap.heap(add, rem, get, set, less, length) -> push, pop  create a heap from an abstract array API
+__cdata heap__
+heap.cdataheap{cmp=f, size=n, ctype=ct|data=cdata} -> h   create a fixed-sized cdata-based heap
+__value heap__
+heap.valueheap{cmp=f[, elem1, ...]} -> h                  create a heap for arbitrary Lua values (except nil)
+__common API__
+h:push(val)                                               push a value
+h:pop() -> val                                            pop highest value
+h:peek() -> val                                           get highest value without popping it
+h:length() -> n                                           number of elements in heap
+--------------------------------------------------------- ----------------------------------------------------
 
-The arg `t` is a pre-sorted binary heap array.
-A comparison function for sorting elements can also be given as `t.cmp`:
+## API Notes
 
-	local h = heap{cmp = function(a, b)
+  * pushing and popping is O(log(n)).
+  * a cdata heap can store size-1 elements.
+  * trying to push nil into a value heap raises an error.
+  * values with the same priority are popped in random order.
+
+## Example:
+
+	local h = heap.valueheap{cmp = function(a, b)
 			return a.priority < b.priority
 		end}
-
-	h:push{priority = 10, etc = 'foo'}
 	h:push{priority = 20, etc = 'bar'}
-
-Pushing and popping is O(log(n)).
-
-Trying to push nil results in error.
-
-Popping values with the same priority does not guarantee insert order.
-For that, provide the elements with an insert counter and use a comparison
-function that compares equal-priority elements against the counter.
+	h:push{priority = 10, etc = 'foo'}
+	assert(h:pop().priority == 10)
